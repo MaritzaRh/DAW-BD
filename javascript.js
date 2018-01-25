4a5026d6f46b8a23bed0a83fd116c8341508a38c
MaritzaVIDEOJUEGOS <A01701357@itesm.mx>
2018-01-24 23:30:23 -0600

    modificar js

--- javascript.js
+++ javascript.js
@@ -1,21 +1,21 @@
 function Estudiante(nombre) {
-    this.nombre = nombre;   
+ 	this.nombre = nombre;   
 }
                 
-    var nombre = prompt("Escribe tu nombre:");
-    var estudiante = new Estudiante(nombre);
+var nombre = prompt("Escribe tu nombre:");
+var estudiante = new Estudiante(nombre);
             
-    Estudiante.prototype.bienvenido = function(){
-        alert('Hola, ¡bienvenido, '+nombre+'!'); 
-    }
+Estudiante.prototype.bienvenido = function(){
+	alert('Hola, ¡bienvenido, '+nombre+'!'); 
+}
             
-    Estudiante.prototype.informacion = function(){
-        var dia = new Date(); 
-        alert('El día de hoy es '+dia.getDate());
-    }
+Estudiante.prototype.informacion = function(){
+	var dia = new Date(); 
+    alert('El día de hoy es '+dia.getDate());
+}
                         
-    estudiante.bienvenido();
-    estudiante.informacion();
+estudiante.bienvenido();
+estudiante.informacion();
 
 var text = document.querySelector('#answer');
 var problema = document.querySelector('#problema');
@@ -63,8 +63,8 @@ var button = document.querySelectorAll("button");
      all.push(negativos.length);
      all.push(ceros.length);
      all.push(positivos.length);
--    var respuesta = "Negativos: " + all[0] + "\n Ceros :" + all[1] + "\n Positivos :" + all[2];
-+    var respuesta = "Negativos: " + all[0] + "<br>Ceros :" + all[1] + "<br> Positivos :" + all[2];
+    var respuesta = "Negativos: " + all[0] + "\n Ceros :" + all[1] + "\n Positivos :" + all[2];
+    var respuesta = "Negativos: " + all[0] + "<br>Ceros :" + all[1] + "<br> Positivos :" + all[2];
  
      return respuesta;
  
@@ -103,8 +103,8 @@ var button = document.querySelectorAll("button");
    all.push(negativos.length);
    all.push(ceros.length);
    all.push(positivos.length);
--  var respuesta = "Negativos: " + all[0] + "\n Ceros :" + all[1] + "\n Positivos :" + all[2];
-+  var respuesta = "Negativos: " + all[0] + "<br> Ceros :" + all[1] + "<br> Positivos :" + all[2] +"<br><br>";
+  var respuesta = "Negativos: " + all[0] + "\n Ceros :" + all[1] + "\n Positivos :" + all[2];
+  var respuesta = "Negativos: " + all[0] + "<br> Ceros :" + all[1] + "<br> Positivos :" + all[2] +"<br><br>";
  
    return respuesta;
  
@@ -138,92 +138,88 @@ var button = document.querySelectorAll("button");
  function selection(name) {
    switch (name) {
      case "button":
-       var letras = document.querySelector('#sentencia').value;
-       var sentencia = new Oracion(letras);
-       sentencia.analiza();
- 
-       break;
-       break;
+		   var letras = document.querySelector('#sentencia').value;
+		   var sentencia = new Oracion(letras);
+		   sentencia.analiza();
+		   break;
      case "button1":
--      problema.textContent = "Entrada: un número pedido con un '/prompt/'. '/Salida/': Una tabla con los números del 1 al número dado con sus cuadrados y '/cubos. Utiliza document.write'/ para producir la salida "
--      text.textContent="";
-+      problema.innerHTML = "Entrada: un número pedido con un '/prompt/'. '/Salida/': Una tabla con los números del 1 al número dado con sus cuadrados y '/cubos. Utiliza document.write'/ para producir la salida "
-+      text.innerHTML="<table>";
-       var num = prompt("Inserta el numero");
-       for (var i = 1; i <= num; i++) {
-         var y = i * i;
--        text.textContent = (text.textContent + "El cuadrado de:" + i + " es: " + y + "\n");
-+        text.innerHTML = ("<tr><td>"+text.innerHTML + "El cuadrado de: " + i + " es: " + y + "<br></td>");
-         y *= i;
--        text.textContent = (text.textContent + "\nEl cubo de:" + i + " es: " + y + "\n");
-+        text.innerHTML = ("<td>"+text.innerHTML + "El cubo de: " + i + " es: " + y + "<br></td></tr>");
-       }
--
-+      text.innerHTML = text.innerHTML + "</table>";
-       break;
+		  problema.textContent = "Entrada: un número pedido con un '/prompt/'. '/Salida/': Una tabla con los números del 1 al número dado con sus cuadrados y '/cubos. Utiliza document.write'/ para producir la salida "
+		  text.textContent="";
+		  problema.innerHTML = "Entrada: un número pedido con un '/prompt/'. '/Salida/': Una tabla con los números del 1 al número dado con sus cuadrados y '/cubos. Utiliza document.write'/ para producir la salida "
+		  text.innerHTML="<table>";
+		  var num = prompt("Inserta el numero");
+		  for (var i = 1; i <= num; i++) {
+			var y = i * i;
+			text.textContent = (text.textContent + "El cuadrado de:" + i + " es: " + y + "\n");
+			text.innerHTML = ("<tr><td>"+text.innerHTML + "El cuadrado de: " + i + " es: " + y + "<br></td>");
+			y *= i;
+			text.textContent = (text.textContent + "\nEl cubo de:" + i + " es: " + y + "\n");
+			text.innerHTML = ("<td>"+text.innerHTML + "El cubo de: " + i + " es: " + y + "<br></td></tr>");
+		   }
+
+		  text.innerHTML = text.innerHTML + "</table>";
+     	  break;
      case "button2":
--      problema.textContent = "Entrada: Usando un prompt se pide el resultado de la suma de 2 números generados de manera aleatoria. Salida: La página debe indicar si el resultado fue correcto o incorrecto, y el tiempo que tardó el usuario en escribir la respuesta."
-+      problema.innerHTML = "Entrada: Usando un prompt se pide el resultado de la suma de 2 números generados de manera aleatoria. Salida: La página debe indicar si el resultado fue correcto o incorrecto, y el tiempo que tardó el usuario en escribir la respuesta."
-       var limit = 100;
-       var num_1 = Math.floor(Math.random() * limit);
-       var num_2 = Math.floor(Math.random() * limit);
-       var total = num_1 + num_2;
-       var date = new Date();
+      	problema.textContent = "Entrada: Usando un prompt se pide el resultado de la suma de 2 números generados de manera aleatoria. Salida: La página debe indicar si el resultado fue correcto o incorrecto, y el tiempo que tardó el usuario en escribir la respuesta."
+      	problema.innerHTML = "Entrada: Usando un prompt se pide el resultado de la suma de 2 números generados de manera aleatoria. Salida: La página debe indicar si el resultado fue correcto o incorrecto, y el tiempo que tardó el usuario en escribir la respuesta."
+        var limit = 100;
+        var num_1 = Math.floor(Math.random() * limit);
+        var num_2 = Math.floor(Math.random() * limit);
+        var total = num_1 + num_2;
+        var date = new Date();
  
-       var seconds = date.getSeconds();
-       var answer = prompt("Inserta la suma de " + num_1 + " y " + num_2);
-       var date2 = new Date();
-       var rsec = date2.getSeconds();
+        var seconds = date.getSeconds();
+        var answer = prompt("Inserta la suma de " + num_1 + " y " + num_2);
+        var date2 = new Date();
+        var rsec = date2.getSeconds();
  
-       var totalTime = rsec - seconds;
-       if (answer == total) {
-         alert("Correcto y tardaste: " + totalTime + " segundos");
-       } else {
-         alert('Incorrecto y tardaste: ' + totalTime + " segundos");
-       }
+        var totalTime = rsec - seconds;
+        if (answer == total) {
+          alert("Correcto y tardaste: " + totalTime + " segundos");
+        } else {
+          alert('Incorrecto y tardaste: ' + totalTime + " segundos");
+        }
  
-       break;
+       	break;
      case "button3":
+      	problema.textContent = "Función: contador. Parámetros: Un arreglo de números. Regresa: La cantidad de números negativos en el arreglo, la cantidad de 0's, y la cantidad de valores mayores a 0 en el arreglo."
+		problema.innerHTML = "Función: contador. Parámetros: Un arreglo de números. Regresa: La cantidad de números negativos en el arreglo, la cantidad de 0's, y la cantidad de valores mayores a 0 en el arreglo.<br>arr = [1, 0, 0, -3, -10, -1, 4, 5]<br>arr = [2, 0, 0, 0, 0, -1, 3, 9]"
+        var arr = [1, 0, 0, -3, -10, -1, 4, 5];
+        text.textContent = (contador(arr));
+        text.innerHTML = (contador(arr));
  
--      problema.textContent = "Función: contador. Parámetros: Un arreglo de números. Regresa: La cantidad de números negativos en el arreglo, la cantidad de 0's, y la cantidad de valores mayores a 0 en el arreglo."
-+      problema.innerHTML = "Función: contador. Parámetros: Un arreglo de números. Regresa: La cantidad de números negativos en el arreglo, la cantidad de 0's, y la cantidad de valores mayores a 0 en el arreglo.<br>arr = [1, 0, 0, -3, -10, -1, 4, 5]<br>arr = [2, 0, 0, 0, 0, -1, 3, 9]"
-       var arr = [1, 0, 0, -3, -10, -1, 4, 5];
--      text.textContent = (contador(arr));
-+      text.innerHTML = (contador(arr));
- 
-       var arr = [2, 0, 0, 0, 0, -1, 3, 9];
--      text.textContent = (text.textContent + "\n" + contador(arr));
-+      text.innerHTML = (text.innerHTML + "\n" + contador(arr));
- 
- 
-       break;
- 
+        var arr = [2, 0, 0, 0, 0, -1, 3, 9];
+        text.textContent = (text.textContent + "\n" + contador(arr));
+        text.innerHTML = (text.innerHTML + "\n" + contador(arr));
+		   
+        break;
  
      case "button4":
--      problema.textContent = "Función: promedios. Parámetros: Un arreglo de arreglos de números. Regresa: Un arreglo con los promedios de cada uno de los renglones de la matriz."
-+      problema.innerHTML = "Función: promedios. Parámetros: Un arreglo de arreglos de números. Regresa: Un arreglo con los promedios de cada uno de los renglones de la matriz. <br> arreglo = [[10, 3, 8],[8, 9, 8]]<br> arreglo = [[10, 9, 9, 9, 8],[8, 6, 5, 4, 9, 8]]"
-       var arreglo = [
-         [10, 3, 8],
-         [8, 9, 8]
-       ];
--      text.textContent = (promedios(arreglo));
-+      text.innerHTML = (promedios(arreglo));
-       var arreglo = [
-         [10, 9, 9, 9, 8],
-         [8, 6, 5, 4, 9, 8]
-       ];
--      text.textContent = (text.textContent + "\n" + promedios(arreglo));
-+      text.innerHTML = (text.innerHTML + "<br>" + promedios(arreglo));
+		 problema.textContent = "Función: promedios. Parámetros: Un arreglo de arreglos de números. Regresa: Un arreglo con los promedios de cada uno de los renglones de la matriz."
+         problema.innerHTML = "Función: promedios. Parámetros: Un arreglo de arreglos de números. Regresa: Un arreglo con los promedios de cada uno de los renglones de la matriz. <br> arreglo = [[10, 3, 8],[8, 9, 8]]<br> arreglo = [[10, 9, 9, 9, 8],[8, 6, 5, 4, 9, 8]]"
+         var arreglo = [
+           [10, 3, 8],
+           [8, 9, 8]
+         ];
+         text.textContent = (promedios(arreglo));
+         text.innerHTML = (promedios(arreglo));
+         var arreglo = [
+           [10, 9, 9, 9, 8],
+           [8, 6, 5, 4, 9, 8]
+         ];
+        text.textContent = (text.textContent + "\n" + promedios(arreglo));
+        text.innerHTML = (text.innerHTML + "<br>" + promedios(arreglo));
  
        break;
-     case "button5":
--      problema.textContent = "Función: inverso. Parámetros: Un número. Regresa: El número con sus dígitos en orden inverso."
--      text.textContent = (inverso(12345));
--      text.textContent = text.textContent + "\n" + inverso(23456);
-+      problema.innerHTML = "Función: inverso. Parámetros: Un número. Regresa: El número con sus dígitos en orden inverso. <br>Inverso(12345) <br>Inverso(23456)"
-+      text.innerHTML = (inverso(12345));
-+      text.innerHTML = text.innerHTML + "<br>" + inverso(23456);
-       break;
+
+	 case "button5":
+         problema.textContent = "Función: inverso. Parámetros: Un número. Regresa: El número con sus dígitos en orden inverso."
+         text.textContent = (inverso(12345));
+         text.textContent = text.textContent + "\n" + inverso(23456);
+         problema.innerHTML = "Función: inverso. Parámetros: Un número. Regresa: El número con sus dígitos en orden inverso. <br>Inverso(12345) <br>Inverso(23456)"
+         text.innerHTML = (inverso(12345));
+         text.innerHTML = text.innerHTML + "<br>" + inverso(23456);
+         break;
  
      default:
  
