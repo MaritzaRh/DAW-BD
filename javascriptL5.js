593a812c1bbf3ba98ce3e3fcdd25cd4a12c07079
MaritzaVIDEOJUEGOS <A01701357@itesm.mx>
2018-01-30 22:04:07 -0600

    LAB5 COMPLETO y Lab6 modificar

--- javascriptL5.js
+++ javascriptL5.js
@@ -1,25 +1,21 @@
- document.querySelector("#r").classList.toggle('error');
+document.querySelector("#r").classList.toggle('error');
 function validate(){
   let xx = document.querySelector('#good');
   let yy = document.querySelector('#bad');
   if(xx.classList.contains('hidden'))
       xx.classList.toggle('hidden');
-      if(yy.classList.contains('hidden'))
-          yy.classList.toggle('hidden');
-
+  if(yy.classList.contains('hidden'))
+      yy.classList.toggle('hidden');
   var password = document.querySelector('#pass1');
   var confirm = document.querySelector('#pass2');
   if(password.value == confirm.value){
     var x = document.querySelector("#bad")
     x.classList.toggle('hidden');
-obtain();
-
-
+	obtain();
   }
   else {
     var x = document.querySelector("#good")
     x.classList.toggle('hidden');
-
   }
 }
 
@@ -40,12 +36,11 @@ function obtain(){
   document.querySelector('#wp').textContent+=pass;
 
   $('.ui.basic.modal')
-  .modal('show')
-;
-document.querySelector('#name').value = "";
-document.querySelector('#tel').value = "";
-document.querySelector('#email').value = "";
-document.querySelector('#pass1').value = "";
+  .modal('show');
+  document.querySelector('#name').value = "";
+  document.querySelector('#tel').value = "";
+  document.querySelector('#email').value = "";
+  document.querySelector('#pass1').value = "";
 }
 
 function comprar() {
@@ -57,15 +52,11 @@ function comprar() {
     var iva;
     var total;
     
-    subtotal = zap*999 + leo*1299 + numLaptop*1999;
+    subtotal = zap*999 + leo*1299 + ves*1999;
     iva = subtotal*0.16;
     total = subtotal+iva;
     
     var div = document.getElementById("cuenta");
-    div.innerHTML = "<strong>Has comprado:</strong><br>";
-    div.innerHTML += zap + " Zapatillas de ballet<br>";
-    div.innerHTML += leo + " Leotardo negro 3<br>";
-    div.innerHTML += ves + " Vestido de ballet<br><br>";
     
     div.innerHTML += "<br><strong>Subtotal:</strong> $ "+ subtotal;
     div.innerHTML += "<br><strong>IVA:</strong> $ "+ iva;
@@ -73,20 +64,31 @@ function comprar() {
 }
 
 
-function adivina(a) {
+function adivina() {
     var nombre = document.getElementById("name").value;
     var edad = document.getElementById("age").value;
     var cumple = document.getElementById("birthday").value;
     
     var miNombre = "Maritza";
     var miEdad = 20;
-    var miCumple = "1997-09-10";
+    var miCumple = "1997-09-10"; 
     
-    if(nombre == miNombre && miEdad == miEdad && cumple == miCumple) {
-        alert("¡Acertaste a todo! ¡Felicidades!");
-        return true;
+    if((nombre == miNombre) && (edad == miEdad) && (cumple == miCumple)){
+        alert('¡Felicidades, ganaste un chocolate!');
     } else { 
-        alert("Fallaste en alguna, intenta de nuevo");
-        return false;
+        alert('Fallaste, intenta de nuevo');
     }
+}
+
+function adivinanza(){
+	document.getElementById("uno").addEventListener("mousedown", mouseDown);
+	document.getElementById("uno").addEventListener("mouseup", mouseUp);
+
+	function mouseDown() {
+		document.getElementById("uno").innerHTML = "El pez";
+	}
+
+	function mouseUp() {
+		document.getElementById("uno").innerHTML = "Pista";
+	}
 }
