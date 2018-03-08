04fd72caaa989bd6de83c6c7e770cab4611293c3
MaritzaVIDEOJUEGOS <A01701357@itesm.mx>
2018-03-08 09:32:31 -0600

    Lab16 TERMINADO

---
+++ DAW/Lab16/ssajax.php
@@ -0,0 +1,19 @@
+<?php
+require_once("util.php");
+$pattern=strtolower($_GET['pattern']);
+$words = getAll();
+$response="";
+$size=0;
+for($i=0; $i<count($words); $i++)
+{
+   $pos=stripos(strtolower($words[$i]),$pattern);
+   if(!($pos===false))
+   {
+     $size++;
+     $word=$words[$i];
+     $response.="<option value=\"$word\">$word</option>";
+   }
+}
+if($size>0)
+  echo "<select class=\"browser-default\" id=\"list\" size=$size onclick=\"selectValue()\">$response</select>";
+?>
